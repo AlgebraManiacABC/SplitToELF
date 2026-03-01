32e016dc824bbf35eab110c295361bf13aab9c3b
AlgebraManiacABC <dunnluke01@gmail.com>
2026-03-01 01:36:57 -0600

    Create basic outline of script

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main(argv: list[str]) -> int:
+    """
+    Main function
+    :param argv: sys.argv
+    :return: exit code
+    """
+
+    # TODO: Get directory with user-compiled objects
+
+    # TODO: Get binary to split
+
+    # TODO: Get symbol list file
+
+    # TODO: Select or create directory where split objects should go
+
+    # TODO: Create list tuple of Address (start, end) which match a binary
+    # TODO: Also keep a list of str which are symbols defined in these binaries and
+    #  which need to be found in the symbol list file to be exported by the split binary
+
+    # TODO: For each valid .o file in user-compiled object directory,
+    #   1. Get list of relocations (for masking during the search)
+    #   2. Find text/data/etc. in main binary (will be a list of matches in case multiple found)
+    #   3. Add imported symbols to list to find later
+
+    # TODO: Create address tuples (start, end) for interstitial space (bytes not matched to a compiled object)
+
+    # TODO: Create object files from interstitial space, exporting symbols which are imported by user-compiled objects
+
+    return 0
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sys.exit(main(sys.argv))
